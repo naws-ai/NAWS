09451570316ab7d7517019b21c6ffbf1e4d42a11
jeon <john@naws.ai>
2024-09-03 23:17:52 +0900

    Update NAWS.sol

--- contracts/NAWS.sol
+++ contracts/NAWS.sol
@@ -1,97 +1,101 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import "./openzeppelin-contracts-5.0.0/token/ERC20/ERC20.sol";
 import "./openzeppelin-contracts-5.0.0/token/ERC20/extensions/ERC20Burnable.sol";
 import "./openzeppelin-contracts-5.0.0/token/ERC20/extensions/ERC20Pausable.sol";
-import "./openzeppelin-contracts-5.0.0/access/Ownable.sol";
+import "./openzeppelin-contracts-5.0.0/access/Ownable.sol"; // Updated Ownable contract
 import "./openzeppelin-contracts-5.0.0/token/ERC20/extensions/ERC20Permit.sol";
 import "./openzeppelin-contracts-5.0.0/token/ERC20/extensions/ERC20Votes.sol";
 import "./openzeppelin-contracts-5.0.0/finance/VestingWallet.sol";
 
 contract NAWS is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit, ERC20Votes {
-
     // Cold wallet addresses (immutable for gas optimization)
-    address public immutable nawsEcosystemColdWallet;
-    address public immutable nawsTeamColdWallet;
-    address public immutable nawsInvestmentColdWallet;
-    address public immutable nawsMarketingColdWallet;
-    address public immutable nawsReserveColdWallet;
+    address public immutable nawsColdWalletDeploy = 0x0E8612586c277e1ca343B9B653bb916B141B8081;
+    address public immutable nawsColdWalletEcosystem = 0xe71277118C276Bef6F722F50F039EfD7aEe5AFAF;
+    address public immutable nawsColdWalletTeam = 0x40ea4678523578839DE6ABcfA74711d38FBd5132;
+    address public immutable nawsColdWalletInvestment = 0xdD668C685d166b950BF3efCb53e49ED9E794976e;
+    address public immutable nawsColdWalletMarketing = 0x9afCD842F6dbCc63C5521E6593DCda5c670F3C4D;
+    address public immutable nawsColdWalletReserve = 0xa9671aA2Ee1AbBC63002053A755642C1A31D9347;
 
     // Vesting wallets for each category
-    VestingWallet public nawsEcosystemVestingContract;
-    VestingWallet public nawsTeamVestingContract;
-    VestingWallet public nawsInvestmentVestingContract;
-    VestingWallet public nawsMarketingVestingContract;
-    VestingWallet public nawsReserveVestingContract;
+    VestingWallet public ecosystemVestingContractWallet;
+    VestingWallet public teamVestingContractWallet;
+    VestingWallet public investmentVestingContractWallet;
+    VestingWallet public marketingVestingContractWallet;
+    VestingWallet public reserveVestingContractWallet;
 
-    error TransferAmountExceedsUnlockedBalance(address from, uint256 requested, uint256 available);
+    // Mapping for banned addresses
+    mapping(address => bool) public banlist;
 
-    constructor(
-            address initialOwner,
-            address _nawsEcosystemColdWallet,
-            address _nawsTeamColdWallet,
-            address _nawsInvestmentColdWallet,
-            address _nawsMarketingColdWallet,
-            address _nawsReserveColdWallet
-        ) ERC20("NAWS", "NAWS") Ownable(initialOwner) ERC20Permit("NAWS") {
-            nawsEcosystemColdWallet = _nawsEcosystemColdWallet; //0xe71277118C276Bef6F722F50F039EfD7aEe5AFAF
-            nawsTeamColdWallet = _nawsTeamColdWallet; //0x40ea4678523578839DE6ABcfA74711d38FBd5132
-            nawsInvestmentColdWallet = _nawsInvestmentColdWallet; //0xdD668C685d166b950BF3efCb53e49ED9E794976e
-            nawsMarketingColdWallet = _nawsMarketingColdWallet; //0x9afCD842F6dbCc63C5521E6593DCda5c670F3C4D
-            nawsReserveColdWallet = _nawsReserveColdWallet; //0xa9671aA2Ee1AbBC63002053A755642C1A31D9347
+    // Events for banning and unbanning addresses
+    event AddressBanned(address indexed account);
+    event AddressUnbanned(address indexed account);
 
+    error AddressIsBanned(address account);
+
+    constructor()
+    ERC20("NAWS", "NAWS")
+    Ownable(nawsColdWalletDeploy) // Use the new constructor for Ownable
+    ERC20Permit("NAWS")
+    {
         uint256 month = 30 days;
+        uint256 totalSupply = 10000000000 * 10 ** decimals(); // Total supply: 10 billion tokens
 
         // Vesting wallets are created with their respective beneficiaries
-        nawsEcosystemVestingContract = new VestingWallet(nawsEcosystemColdWallet, uint64(block.timestamp), uint64(month * 18));
-        nawsTeamVestingContract = new VestingWallet(nawsTeamColdWallet, uint64(block.timestamp), uint64(month * 24));
-        nawsInvestmentVestingContract = new VestingWallet(nawsInvestmentColdWallet, uint64(block.timestamp), uint64(month * 24));
-        nawsMarketingVestingContract = new VestingWallet(nawsMarketingColdWallet, uint64(block.timestamp), uint64(month * 36));
-        nawsReserveVestingContract = new VestingWallet(nawsReserveColdWallet, uint64(block.timestamp), uint64(month * 36));
+        // Vesting starts based on TGE (Token Generation Event)
+        ecosystemVestingContractWallet = new VestingWallet(nawsColdWalletEcosystem, uint64(block.timestamp), uint64(month * 18)); // 18 months vesting
+        teamVestingContractWallet = new VestingWallet(nawsColdWalletTeam, uint64(block.timestamp), uint64(month * 24)); // 24 months vesting
+        investmentVestingContractWallet = new VestingWallet(nawsColdWalletInvestment, uint64(block.timestamp), uint64(month * 24)); // 24 months vesting
+        marketingVestingContractWallet = new VestingWallet(nawsColdWalletMarketing, uint64(block.timestamp), uint64(month * 36)); // 36 months vesting
+        reserveVestingContractWallet = new VestingWallet(nawsColdWalletReserve, uint64(block.timestamp), uint64(month * 36)); // 36 months vesting
+
+        // Calculate 20% of total supply for each wallet
+        uint256 allocationAmount = totalSupply / 5; // 20% allocation per wallet (2 billion tokens each)
 
         // Initial token allocation to cold wallets
-        _mint(nawsEcosystemColdWallet, 1000000000 * 10 ** decimals());
-        _mint(nawsTeamColdWallet, 500000000 * 10 ** decimals());
-        _mint(nawsInvestmentColdWallet, 500000000 * 10 ** decimals());
-        _mint(nawsMarketingColdWallet, 500000000 * 10 ** decimals());
-        _mint(nawsReserveColdWallet, 0);
+        _mint(nawsColdWalletEcosystem, allocationAmount / 2); // Immediate release of 1 billion tokens to Ecosystem cold wallet
+        _mint(nawsColdWalletTeam, allocationAmount / 4); // Immediate release of 500 million tokens to Team cold wallet
+        _mint(nawsColdWalletInvestment, allocationAmount / 4); // Immediate release of 500 million tokens to Investment cold wallet
+        _mint(nawsColdWalletMarketing, allocationAmount / 4); // Immediate release of 500 million tokens to Marketing cold wallet
+        // Reserve wallet initial allocation is 0 tokens to the cold wallet
+
+        // Vesting wallet allocation for the locked amounts
+        _mint(address(ecosystemVestingContractWallet), allocationAmount / 2); // 1 billion tokens to Ecosystem vesting contract wallet
+        _mint(address(teamVestingContractWallet), allocationAmount * 3 / 4); // 1.5 billion tokens to Team vesting contract wallet
+        _mint(address(investmentVestingContractWallet), allocationAmount * 3 / 4); // 1.5 billion tokens to Investment vesting contract wallet
+        _mint(address(marketingVestingContractWallet), allocationAmount * 3 / 4); // 1.5 billion tokens to Marketing vesting contract wallet
+        _mint(address(reserveVestingContractWallet), allocationAmount); // 2 billion tokens to Reserve vesting contract wallet
     }
 
-    // Function to check timelocks before transferring tokens
-    function _checkTimelocks(address from, uint256 amount) internal view {
-        uint256 lockedAmount = 0;
-        uint64 currentTime = uint64(block.timestamp);
-
-        if (from == nawsEcosystemColdWallet) {
-            lockedAmount = nawsEcosystemVestingContract.vestedAmount(address(this), currentTime) - nawsEcosystemVestingContract.released(address(this));
-        } else if (from == nawsTeamColdWallet) {
-            lockedAmount = nawsTeamVestingContract.vestedAmount(address(this), currentTime) - nawsTeamVestingContract.released(address(this));
-        } else if (from == nawsInvestmentColdWallet) {
-            lockedAmount = nawsInvestmentVestingContract.vestedAmount(address(this), currentTime) - nawsInvestmentVestingContract.released(address(this));
-        } else if (from == nawsMarketingColdWallet) {
-            lockedAmount = nawsMarketingVestingContract.vestedAmount(address(this), currentTime) - nawsMarketingVestingContract.released(address(this));
-        } else if (from == nawsReserveColdWallet) {
-            lockedAmount = nawsReserveVestingContract.vestedAmount(address(this), currentTime) - nawsReserveVestingContract.released(address(this));
-        }
-
-        uint256 available = balanceOf(from) - lockedAmount;
-        if (available < amount) {
-            revert TransferAmountExceedsUnlockedBalance(from, amount, available);
-        }
+    // Modifier to check if an address is banned
+    modifier notBanned(address account) {
+        require(!banlist[account], "NAWS: Address is banned");
+        _;
     }
 
     function _update(address from, address to, uint256 value)
         internal
         override(ERC20, ERC20Pausable, ERC20Votes)
+        whenNotPaused
+        notBanned(from)
+        notBanned(to)
     {
-        if (from != address(0)) {
-            // Only check timelocks if tokens are being transferred out
-            _checkTimelocks(from, value);
-        }
         super._update(from, to, value);
     }
 
+    // Functions to ban and unban addresses
+    function banAddress(address account) public onlyOwner {
+        banlist[account] = true;
+        emit AddressBanned(account);
+    }
+
+    function unbanAddress(address account) public onlyOwner {
+        banlist[account] = false;
+        emit AddressUnbanned(account);
+    }
+
+    // Pausing and unpausing contract functions
     function pause() public onlyOwner {
         _pause();
     }
