3f45d6b757b5e5ea2467b6d6647be1c1661cd287
jeon <john@naws.ai>
2024-09-12 14:56:19 +0900

    Separate vesting logic

--- contracts/NAWS.sol
+++ contracts/NAWS.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.26;
 
 import "./openzeppelin-contracts-5.0.0/token/ERC20/ERC20.sol";
 import "./openzeppelin-contracts-5.0.0/token/ERC20/extensions/ERC20Burnable.sol";
@@ -7,7 +7,6 @@ import "./openzeppelin-contracts-5.0.0/token/ERC20/extensions/ERC20Pausable.sol"
 import "./openzeppelin-contracts-5.0.0/access/Ownable.sol";
 import "./openzeppelin-contracts-5.0.0/token/ERC20/extensions/ERC20Permit.sol";
 import "./openzeppelin-contracts-5.0.0/token/ERC20/extensions/ERC20Votes.sol";
-import "./openzeppelin-contracts-5.0.0/finance/VestingWallet.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract NAWS is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit, ERC20Votes, ReentrancyGuard {
@@ -18,13 +17,6 @@ contract NAWS is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit, ERC2
     address public immutable nawsColdWalletInvestment = 0xdD668C685d166b950BF3efCb53e49ED9E794976e;
     address public immutable nawsColdWalletMarketing = 0x9afCD842F6dbCc63C5521E6593DCda5c670F3C4D;
     address public immutable nawsColdWalletReserve = 0xa9671aA2Ee1AbBC63002053A755642C1A31D9347;
-
-    // Vesting wallets for each category
-    VestingWallet public ecosystemVestingContractWallet;
-    VestingWallet public teamVestingContractWallet;
-    VestingWallet public investmentVestingContractWallet;
-    VestingWallet public marketingVestingContractWallet;
-    VestingWallet public reserveVestingContractWallet;
 
     // Constants for token distribution
     uint256 public constant TOTAL_SUPPLY = 10000000000 * 10 ** 18; // Total supply: 10 billion tokens
@@ -54,37 +46,20 @@ contract NAWS is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit, ERC2
         require(nawsColdWalletMarketing != address(0), "Invalid marketing wallet address");
         require(nawsColdWalletReserve != address(0), "Invalid reserve wallet address");
 
-        // Initialize vesting wallets
-        uint256 month = 30 days;
-        ecosystemVestingContractWallet = new VestingWallet(nawsColdWalletEcosystem, uint64(block.timestamp), uint64(month * 18));
-        teamVestingContractWallet = new VestingWallet(nawsColdWalletTeam, uint64(block.timestamp), uint64(month * 24));
-        investmentVestingContractWallet = new VestingWallet(nawsColdWalletInvestment, uint64(block.timestamp), uint64(month * 24));
-        marketingVestingContractWallet = new VestingWallet(nawsColdWalletMarketing, uint64(block.timestamp), uint64(month * 36));
-        reserveVestingContractWallet = new VestingWallet(nawsColdWalletReserve, uint64(block.timestamp), uint64(month * 36));
-
         // Token distribution
         _mint(address(this), TOTAL_SUPPLY);
         _initializeColdWallets();
-        _initializeVestingWallets();
     }
 
     function _initializeColdWallets() internal {
         // Distribute initial tokens to cold wallets
-        _transfer(address(this), nawsColdWalletEcosystem, ALLOCATION_AMOUNT / 2);
-        _transfer(address(this), nawsColdWalletTeam, ALLOCATION_AMOUNT / 4);
-        _transfer(address(this), nawsColdWalletInvestment, ALLOCATION_AMOUNT / 4);
-        _transfer(address(this), nawsColdWalletMarketing, ALLOCATION_AMOUNT / 4);
+        _transfer(address(this), nawsColdWalletEcosystem, ALLOCATION_AMOUNT);
+        _transfer(address(this), nawsColdWalletTeam, ALLOCATION_AMOUNT);
+        _transfer(address(this), nawsColdWalletInvestment, ALLOCATION_AMOUNT);
+        _transfer(address(this), nawsColdWalletMarketing, ALLOCATION_AMOUNT);
+        _transfer(address(this), nawsColdWalletReserve, ALLOCATION_AMOUNT);
     }
     
-    function _initializeVestingWallets() internal {
-        // Distribute tokens to vesting contract wallets
-        _transfer(address(this), address(ecosystemVestingContractWallet), ALLOCATION_AMOUNT / 2); // 1 billion tokens
-        _transfer(address(this), address(teamVestingContractWallet), (ALLOCATION_AMOUNT / 4) * 3);  // 1.5 billion tokens
-        _transfer(address(this), address(investmentVestingContractWallet), (ALLOCATION_AMOUNT / 4) * 3); // 1.5 billion tokens
-        _transfer(address(this), address(marketingVestingContractWallet), (ALLOCATION_AMOUNT / 4) * 3); // 1.5 billion tokens
-        _transfer(address(this), address(reserveVestingContractWallet), ALLOCATION_AMOUNT);       // 2 billion tokens
-    }
-
     modifier notBanned(address account) {
         require(!banlist[account], "NAWS: Address is banned");
         _;
